9a30c6c8d725c456b20db04f408b9485fc7e7b30
Joseph Pendon <pendon.josephjr@gmail.com>
2026-01-18 13:21:09 +0000

    Made the calculator infinite run logic and added addition function

---
+++ parallel.py
@@ -0,0 +1,38 @@
+def add(a, b):
+    return a + b
+
+print("=== Persistent Calculator ===")
+print("Examples: 1+2 | 11*12 | 1 - 3 | 4/1")
+print("Type 'exit' to stop.\n")
+
+while True:
+    expr = input("Enter Values: ")
+
+    if expr.lower() == "exit":
+        print("Calculator stopped.")
+        break
+
+    try:
+        # Remove all spaces
+        expr = expr.replace(" ", "")
+
+        # Find operator
+        for op in ["+"]:
+            if op in expr:
+                num1, num2 = expr.split(op, 1)
+                operator = op
+                break
+        else:
+            print("Invalid expression\n")
+            continue
+
+        num1 = float(num1)
+        num2 = float(num2)
+
+        match operator:
+            case "+":
+                result = add(num1, num2)
+        print(f"Result: {result}\n")
+
+    except ValueError:
+        print("Invalid input\n")
