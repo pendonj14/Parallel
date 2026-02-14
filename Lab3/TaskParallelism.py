1ea449f44cf8dc5f09be05e033dc683529c8c66a
Joseph Pendon <pendon.josephjr@gmail.com>
2026-02-14 13:55:10 +0000

    task parallelism part 2

--- Lab3/TaskParallelism.py
+++ Lab3/TaskParallelism.py
@@ -1,3 +1,4 @@
+from concurrent.futures import ThreadPoolExecutor
 
 def compute_sss(salary):
     return salary * 0.045
@@ -10,3 +11,31 @@ def compute_pagibig(salary):
 
 def compute_tax(salary):
     return salary * 0.10
+
+
+def main():
+    salary = 30000
+
+    with ThreadPoolExecutor() as executor:
+        futures = [
+            executor.submit(compute_sss, salary),
+            executor.submit(compute_philhealth, salary),
+            executor.submit(compute_pagibig, salary),
+            executor.submit(compute_tax, salary)
+        ]
+
+        results = [f.result() for f in futures]
+
+    sss, philhealth, pagibig, tax = results
+    total_deduction = sum(results)
+
+    print("Salary:", salary)
+    print("SSS:", sss)
+    print("PhilHealth:", philhealth)
+    print("Pag-IBIG:", pagibig)
+    print("Tax:", tax)
+    print("Total Deduction:", total_deduction)
+
+
+if __name__ == "__main__":
+    main()
