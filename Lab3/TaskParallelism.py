735f19a97b20af62b73c5b6e7b0aa4da8358bf72
kent chavo <chavo.kentjohn@gmail.com>
2026-02-14 13:53:57 +0000

    task parellelism part 1

---
+++ Lab3/TaskParallelism.py
@@ -0,0 +1,12 @@
+
+def compute_sss(salary):
+    return salary * 0.045
+
+def compute_philhealth(salary):
+    return salary * 0.025
+
+def compute_pagibig(salary):
+    return salary * 0.02
+
+def compute_tax(salary):
+    return salary * 0.10
