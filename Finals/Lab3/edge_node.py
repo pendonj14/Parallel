fe9fc86926a2185e82c7f720a256f788c61f84b5
Richter Anthony P. Yap <yap.richtera@gmail.com>
2026-05-09 13:27:51 +0000

    added function generate_vote

--- Finals/Lab3/edge_node.py
+++ Finals/Lab3/edge_node.py
@@ -10,5 +10,12 @@ API_URL = "https://vote-api-108304418208.asia-southeast1.run.app/vote"
 # Each group member uses a different node_id (1, 2, 3, or 4)
 NODE_ID = int(sys.argv[1]) if len(sys.argv) > 1 else 1
 
-
+def generate_vote():
+    return {
+        "user_id": str(uuid.uuid4()),
+        "poll_id": "poll_1",
+        "choice": random.choice(["A", "B", "C"]),
+        "edge_id": f"node_{NODE_ID}",
+        "timestamp": time.time()
+    }
 
